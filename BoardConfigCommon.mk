87c554aa3d17af50b3b4f27f77ed80b866bfc880
RaiMaru24 <alenbijuk2003@gmail.com>
2025-02-25 14:45:10 +0000

    sm6150-common: Inherit MiuiCamera
    Signed-off-by: RaiMaru24 <alenbijuk2003@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -19,6 +19,9 @@ COMMON_PATH := device/xiaomi/sm6150-common
 # Inherit from proprietary files
 include vendor/xiaomi/sm6150-common/BoardConfigVendor.mk
 
+# MiuiCamera
+-include device/xiaomi/miuicamera-sweet/BoardConfig.mk
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
--- sm6150.mk
+++ sm6150.mk
@@ -28,6 +28,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Call the MiuiCamera setup
+$(call inherit-product-if-exists, device/xiaomi/miuicamera-sweet/device.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
